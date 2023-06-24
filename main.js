131595261c02e7d3960f306f273a0cfc956f3087
armando <armando@pop-os.localdomain>
2023-06-24 14:18:44 -0700

    added script

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 import './style.css'
 
+const defaultDate = new Date()
+
+document.getElementById('day').addEventListener('input', (event) => { 
+  document.getElementById('days-result').innerText = defaultDate.getDate() - Number(event.target.value)
+})
+
+document.getElementById('month').addEventListener('input', (event) => { 
+  document.getElementById('months-result').innerText = Math.abs(defaultDate.getMonth() + 1 - Number(event.target.value))
+})
+
+document.getElementById('year').addEventListener('input', (event) => { 
+  document.getElementById('years-result').innerText = defaultDate.getFullYear() - Number(event.target.value)
+})

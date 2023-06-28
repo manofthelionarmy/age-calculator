6c285f78094be6d9f43c9b8c1c80ac75ebb10eeb
armando <armando@pop-os.localdomain>
2023-06-27 19:12:02 -0700

    began working on displaying error label under input

--- main.js
+++ main.js
@@ -13,3 +13,27 @@ document.getElementById('month').addEventListener('input', (event) => {
 document.getElementById('year').addEventListener('input', (event) => { 
   document.getElementById('years-result').innerText = defaultDate.getFullYear() - Number(event.target.value)
 })
+
+document.getElementById('form-control').addEventListener('submit', (event) => {
+  event.preventDefault()
+  const year = document.getElementById('year')
+  const month = document.getElementById('month')
+  const day = document.getElementById('day')
+  for ( const elem of [year, month, day] ) {
+    if ( elem.nextElementSibling !== null)  {
+      elem.nextElementSibling.remove()
+    }
+    if ( !validInput(elem) ) {
+      elem.insertAdjacentHTML('afterend', `<p class="error">hello</p>`)
+      return
+    }
+  }
+})
+
+function validInput(elem) {
+  const {tooShort, tooLong} = elem.validity
+  if (tooShort || tooLong) {
+    return false
+  }
+  return true
+}
